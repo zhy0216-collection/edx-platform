0b023441aba3f0ba6a7c420799735112c885b516
cahrens <christina@edx.org>
2016-01-08 10:02:56 -0500

    cleanup

--- lms/djangoapps/instructor_task/tests/test_tasks_helper.py
+++ lms/djangoapps/instructor_task/tests/test_tasks_helper.py
@@ -1648,7 +1648,7 @@ class TestCertificateGeneration(InstructorTaskModuleTestCase):
         current_task.update_state = Mock()
         instructor_task = Mock()
         instructor_task.task_input = json.dumps({'students': None})
-        with self.assertNumQueries(214):  # Added by Christina to get tests passing again-- think about this.
+        with self.assertNumQueries(213):
             with patch('instructor_task.tasks_helper._get_current_task') as mock_current_task:
                 mock_current_task.return_value = current_task
                 with patch('capa.xqueue_interface.XQueueInterface.send_to_queue') as mock_queue:
